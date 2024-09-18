a472a4e504df08e266cb09c1bf173ff17d06a5a9
Slava Zakharov <slava.zakharov@mailbox.org>
2024-09-18 16:01:14 +0400

    Rework ex3 ch11 in true async manner

--- 11ch/v4/promise-all.js
+++ 11ch/v4/promise-all.js
@@ -1,20 +1,36 @@
 function Promise_all(promises) {
-	return new Promise((resolve, reject) => {
+	return new Promise((doRes, doRej) => {
+		if (!Array.isArray(promises)) return doRej({err: "Input isn't an Array"});
 		const resolvedValues = [];
 
-		const iterator = (i) => {
-			if (i === promises.length) return resolve(resolvedValues);
+		if (promises.length === 0) return doRes(resolvedValues);
 
-			const toExecute = promises[i];
-			toExecute
+		let pending = promises.length;
+
+		for (let i = 0; i < promises.length; ++i) {
+			promises[i]
 				.then((res) => {
-					resolvedValues.push(res);
-					return iterator(++i);
-				})
-				.catch((err) => reject({err}));
-		};
+					resolvedValues[i] = res;
+					--pending;
 
-		iterator(0);
+					if (pending === 0) doRes(resolvedValues);
+				})
+				.catch((err) => doRej({err}));
+		}
+
+		// const iterator = (i) => {
+		// 	if (i === promises.length) return doRes(resolvedValues);
+
+		// 	const toExecute = promises[i];
+		// 	toExecute
+		// 		.then((res) => {
+		// 			resolvedValues.push(res);
+		// 			return iterator(++i);
+		// 		})
+		// 		.catch((err) => doRej({err}));
+		// };
+
+		// iterator(0);
 	});
 }
 
@@ -23,14 +39,14 @@ Promise_all([]).then((array) => {
 	console.log("This should be []:", array);
 });
 function soon(val) {
-	return new Promise((resolve) => {
-		setTimeout(() => resolve(val), Math.random() * 500);
+	return new Promise((doRes) => {
+		setTimeout(() => doRes(val), Math.random() * 500);
 	});
 }
 Promise_all([soon(1), soon(2), soon(3)]).then((array) => {
 	console.log("This should be [1, 2, 3]:", array);
 });
-Promise_all([soon(1), Promise.reject("X"), soon(3)])
+Promise_all([soon(1), Promise.doRej("X"), soon(3)])
 	.then((array) => {
 		console.log("We should not get here");
 	})
